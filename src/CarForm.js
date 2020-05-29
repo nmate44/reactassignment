d004b83f7c68f2501e9ac9625941ac963ae3a832
nmate44 <nemethmate414@gmail.com>
2020-05-29 21:41:36 +0200

    CarForm logikája, connect és dispatch kész

--- src/CarForm.js
+++ src/CarForm.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class CarForm extends Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const manufacturer = this.getManufacturer.value;
+        const name = this.getName.value;
+        const horsepower = this.getHorsepower.value;
+
+        const data = {
+            id: new Date(),
+            manufacturer,
+            name,
+            horsepower
+        }
+        console.log(data);
+        this.props.dispatch({type:'ADD_CAR', data});
+        this.getManufacturer.value = '';
+        this.getName.value = '';
+        this.getHorsepower.value = '';
+    }
+
     render() {
         return (
             <div>
                 <h2>Add a Car</h2>
-                <form>
-                    <input required type="text" placeholder="Manufacturer"/><br />
-                    <input required type="text" placeholder="Name"/><br />
-                    <input required type="number" placeholder="Horsepower"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input required type="text" ref={(input) => this.getManufacturer = input} placeholder="Manufacturer"/><br />
+                    <input required type="text" ref={(input) => this.getName = input} placeholder="Name"/><br />
+                    <input required type="number" ref={(input) => this.getHorsepower = input} placeholder="Horsepower"/><br />
+                    <button>Add Car</button>
                 </form>
             </div>
         );
     }
 }
 
-export default CarForm;
+export default connect()(CarForm);
