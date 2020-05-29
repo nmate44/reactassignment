af9a06ead43a9c202702f128077c65536ba16971
nmate44 <nemethmate414@gmail.com>
2020-05-29 21:15:42 +0200

    Provider alapja kész

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import './index.css';
 import App from './App';
 
 import { createStore } from "redux";
+import {Provider} from "react-redux";
 import carReducer from './reducers/carReducer'
 
 
 const store = createStore(carReducer);
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
